3d2a24484bc417e66561b3f4979ebd745b855eeb
lokeshloki3 <lokesh1992p@gmail.com>
2025-06-12 16:54:10 +0530

    initial pagination setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Pagination from "./components/Pagination";
+
+function App() {
+
+  return (
+    <div className="h-screen p-4">
+        <Pagination />
+    </div>
+  )
+}
+
+export default App
